780984872f815de034fa559ca86cbc5a42c24a9b
Raju Talukder <63764709+Mostafaarju@users.noreply.github.com>
2022-02-27 18:37:29 +0100

    first commit

---
+++ src/robots.js
@@ -0,0 +1,72 @@
+export const robots = [
+    {
+        id:1 ,
+        name:'Mary Singleton',
+        userName: 'Hernandez',
+        email: 'pitva@zevi.dm',
+    
+    },
+    {
+        id:2,
+        name:'Hallie McDonald',
+        userName: 'Summers',
+        email: 'ligufobep@uliabduk.pa',
+    
+    },
+    {
+        id: 3,
+        name:'Gerald Daniel',
+        userName: 'Cortez',
+        email: 'im@fin.ng',
+    
+    },
+    {
+        id:4 ,
+        name:'Dale Wilkerson',
+        userName: 'Day',
+        email: 'lo@pukraj.nc',
+    
+    },
+    {
+        id:5 ,
+        name:'Susie Brock',
+        userName: 'Burgess',
+        email: 'zarani@ludicepi.gov',
+    
+    },
+    {
+        id:6 ,
+        name:'Rodney Reid',
+        userName: 'James',
+        email: 'ivocevo@us.is',
+    
+    },
+    {
+        id:7 ,
+        name:'Francis Rodgers',
+        userName: 'Collier',
+        email: 'hatmaroti@vobobu.sj',
+    
+    },
+    {
+        id:8 ,
+        name:'Barry Glover',
+        userName: 'Harmon',
+        email: 'om@osatopit.lv',
+    
+    },
+    {
+        id:9 ,
+        name:'Julian Jackson',
+        userName: 'Tucker',
+        email: 'mi@uwasa.is',
+    
+    },
+    {
+        id:10 ,
+        name:'Nelle Ramos',
+        userName: 'Harrison',
+        email: 'istewde@rubsaztug.az',
+    
+    },
+]
